066dff9343b894ac6f7f5baab6e5897debd3ae00
Anton Solodkov <solodkovantosha@gmail.com>
2024-04-04 19:20:09 +0300

    add: new env contants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,1 @@
-export const { VITE_TILE_LAYER_URL } = import.meta.env;
+export const { VITE_TILE_LAYER_URL, VITE_PLACES_API_KEY, VITE_PLACES_API_URL } = import.meta.env;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_TILE_LAYER_URL: string
+    readonly VITE_TILE_LAYER_URL: string,
+    readonly VITE_PLACES_API_KEY: string,
+    readonly VITE_PLACES_API_URL: string,
   }
   
   interface ImportMeta {
-    readonly env: ImportMetaEnv
+    readonly env: ImportMetaEnv,
   }
