d08a64c9b217b398f6feb120e3b019a35b0bfeb8
Anton Solodkov <solodkovantosha@gmail.com>
2024-04-06 10:55:03 +0300

    add: placeOptions

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,7 +1,46 @@
 import favoritesImg from '@assets/img/favorites.svg';
 import logInImg from '@assets/img/logIn.svg';
 import logoImg from '@assets/img/logo.svg';
+import adultImg from '@assets/img/placeIcons/18+.svg';
+import amusementsImg from '@assets/img/placeIcons/amusements.svg';
+import archtectureImg from '@assets/img/placeIcons/architecture.svg';
+import bankImg from '@assets/img/placeIcons/bank.svg';
+import bicycleImg from '@assets/img/placeIcons/bicycle.svg';
+import carImg from '@assets/img/placeIcons/car.svg';
+import coffeeImg from '@assets/img/placeIcons/coffee.svg';
+import cultureImg from '@assets/img/placeIcons/culture.svg';
+import foodImg from '@assets/img/placeIcons/food.svg';
+import gasStationImg from '@assets/img/placeIcons/gas_station.svg';
+import historyImg from '@assets/img/placeIcons/history.svg';
+import hotelsImg from '@assets/img/placeIcons/hotels.svg';
+import industrialImg from '@assets/img/placeIcons/industrial_facilities.svg';
+import natureImg from '@assets/img/placeIcons/nature.svg';
+import otherImg from '@assets/img/placeIcons/other.svg';
+import religionImg from '@assets/img/placeIcons/religion.svg';
+import shopImg from '@assets/img/placeIcons/shop.svg';
+import sportImg from '@assets/img/placeIcons/sport.svg';
 import searchImg from '@assets/img/search.svg';
 import locationImg from '@assets/img/tabler_location-filled.svg';
 
-export { favoritesImg, locationImg, logInImg, logoImg, searchImg };
+export { favoritesImg, locationImg,logInImg, logoImg, searchImg };
+
+export const placesImg = {
+  adultImg,
+  amusementsImg,
+  archtectureImg,
+  bankImg,
+  bicycleImg,
+  carImg,
+  coffeeImg,
+  cultureImg,
+  foodImg,
+  gasStationImg,
+  historyImg,
+  hotelsImg,
+  industrialImg,
+  natureImg,
+  otherImg,
+  religionImg,
+  shopImg,
+  sportImg,
+};
